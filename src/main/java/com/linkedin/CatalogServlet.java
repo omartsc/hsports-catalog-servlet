177bc27bd38b8284378780fbd06293bc9bd32187
Omar Tapia Santacruz <omar.tsc@gmail.com>
2021-04-10 20:45:25 +0200

    add JSP

--- src/main/java/com/linkedin/CatalogServlet.java
+++ src/main/java/com/linkedin/CatalogServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.AsyncContext;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
@@ -17,7 +18,7 @@ import com.linkedin.servlet.CatalogItem;
 /**
  * Servlet implementation class CatalogServlet
  */
-@WebServlet(urlPatterns="/CatalogServlet", asyncSupported = true)
+@WebServlet(urlPatterns="/CatalogServlet")
 public class CatalogServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -41,37 +42,6 @@ public class CatalogServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		AsyncContext asyncContext = request.startAsync();
-		
-		asyncContext.start(new Runnable() {
-
-			@Override
-			public void run() {
-				
-				try {
-					Thread.sleep(5000);
-					System.out.println("Print the response");
-					System.out.println("response returned by " + Thread.currentThread().getName());
-					returnResponse(request, response);
-					asyncContext.complete();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			}
-			
-			
-		});
-		
-		System.out.println("Initial Request: " + Thread.currentThread().getName());
-		
-	}
-
-	private void returnResponse(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String name = request.getParameter("name");
 		String manufacturer = request.getParameter("manufacturer");
 		String sku = request.getParameter("sku");
@@ -81,24 +51,10 @@ public class CatalogServlet extends HttpServlet {
 		response.setHeader("someHeader", "someHeaderValue");
 		response.addCookie(new Cookie("someCookie", "someCookieValue"));
 		
-		PrintWriter out = response.getWriter();
+		request.setAttribute("message", name);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("list.jsp");
+		dispatcher.forward(request, response);
 		
-		out.println("<html>");
-		out.println("<head></head>");
-		out.println("<body>");
-		out.println("<table>");
-		
-		for(CatalogItem item: Catalog.getItems()) {
-			out.println("<tr>");
-			out.println("<td>");
-			out.println(item.getName());
-			out.println("</td>");
-			out.println("</tr>");
-		}
-		
-		out.println("</table>");
-		out.println("</body>");
-		out.println("</html>");
 	}
 
 }
