1411b11cbe26780248c726b569cde5e1335afd82
Omar Tapia Santacruz <omar.tsc@gmail.com>
2021-04-10 20:27:47 +0200

    add asynchronous support

--- src/main/java/com/linkedin/CatalogServlet.java
+++ src/main/java/com/linkedin/CatalogServlet.java
@@ -3,6 +3,7 @@ package com.linkedin;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
@@ -16,7 +17,7 @@ import com.linkedin.servlet.CatalogItem;
 /**
  * Servlet implementation class CatalogServlet
  */
-@WebServlet("/CatalogServlet")
+@WebServlet(urlPatterns="/CatalogServlet", asyncSupported = true)
 public class CatalogServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -39,6 +40,38 @@ public class CatalogServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		AsyncContext asyncContext = request.startAsync();
+		
+		asyncContext.start(new Runnable() {
+
+			@Override
+			public void run() {
+				
+				try {
+					Thread.sleep(5000);
+					System.out.println("Print the response");
+					System.out.println("response returned by " + Thread.currentThread().getName());
+					returnResponse(request, response);
+					asyncContext.complete();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			
+			
+		});
+		
+		System.out.println("Initial Request: " + Thread.currentThread().getName());
+		
+	}
+
+	private void returnResponse(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String name = request.getParameter("name");
 		String manufacturer = request.getParameter("manufacturer");
 		String sku = request.getParameter("sku");
--- src/main/java/com/linkedin/servlet/ParameterLoggingFilter.java
+++ src/main/java/com/linkedin/servlet/ParameterLoggingFilter.java
@@ -12,7 +12,7 @@ import javax.servlet.annotation.WebFilter;
 /**
  * Servlet Filter implementation class ParameterLoggingFilter
  */
-@WebFilter("/*")
+@WebFilter(urlPatterns="/*", asyncSupported=true)
 public class ParameterLoggingFilter implements Filter {
 
     /**
